843bf8604108c33251af92d373839232f19a0af2
abdurauf0788 <turkeynumber063@gmail.com>
2025-12-27 19:07:00 +0500

    JWT token auth

---
+++ accounts/permissions.py
@@ -0,0 +1,21 @@
+from rest_framework.permissions import BasePermission
+
+class IsAdmin(BasePermission):
+    message = "siz admin emassiz"
+
+    def has_permission(self, request, view):
+        return request.user and request.user.is_admin
+    
+class IsUser(BasePermission):
+    message = "siz admin emassiz"
+
+    def has_permission(self, request, view):
+        return request.user and request.user.is_user
+    
+
+class IsManager(BasePermission):
+    message = "siz admin emassiz"
+
+    def has_permission(self, request, view):
+        return request.user and request.user.is_manager
+    
--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from accounts.permissions import IsAdmin
+
+class Profile(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated, IsAdmin]
+
+    def get(self, request:Request)->Response:
+        return Response({"message":"Salom Dunyo"})
