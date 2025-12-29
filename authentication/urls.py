f235d1f545ef32cb239d26efa526b2649129c0f4
abdurauf0788 <turkeynumber063@gmail.com>
2025-12-29 11:27:06 +0500

    get access token url

--- authentication/urls.py
+++ authentication/urls.py
@@ -5,10 +5,15 @@ from rest_framework_simplejwt.views import (
     TokenVerifyView,
 )
 
-from .views import Profile
+
+from .views import Profile, GoogleCallbackView, GoogleLoginView
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
-    path('profile/', Profile.as_view(), name="user_profile")
+    path('profile/', Profile.as_view(), name="user_profile"),
+
+    path('google/login/', GoogleLoginView.as_view(), name="google-view"),
+    path('google/callback/', GoogleCallbackView.as_view(), name="callback"),
+
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,8 +1,13 @@
+import requests
+
 from rest_framework.views import APIView
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
+
+from django.conf import settings
 
 from accounts.permissions import IsAdmin
 
@@ -12,3 +17,35 @@ class Profile(APIView):
 
     def get(self, request:Request)->Response:
         return Response({"message":"Salom Dunyo"})
+    
+
+class GoogleLoginView(APIView):
+    def post(self, request:  Request)-> Response:
+        url = f'https://accounts.google.com/o/oauth2/v2/auth?'\
+                f'client_id={settings.GOOGLE_CLIENT_ID}&'\
+                f'redirect_uri={settings.GOOGLE_REDIRECT_URL}&'\
+                f'scope=email profile&'\
+                f'response_type=code&'
+        
+        return Response({
+            "message":"Google Auth uchun link",
+            "link" : url
+        })
+
+
+class GoogleCallbackView(APIView):
+    def get(self, request:  Request)-> Response:
+        code = request.query_params.get('code')
+
+        if code is None:
+            return Response({"error":"code is required"}, status=status.HTTP_401_UNAUTHORIZED)
+        data = {
+            "grant_type":"authorization_code",
+            "client_id": settings.GOOGLE_CLIENT_ID,
+            "client_secret":settings.GOOGLE_CLIENT_SECRET,
+            "code": code,
+            "redirect_uri":settings.GOOGLE_REDIRECT_URL
+        }
+        access_token = requests.post(settings.GOOGLE_TOKEN_URL, data=data).json()['access_token']
+        
+        return Response({"token": access_token})
